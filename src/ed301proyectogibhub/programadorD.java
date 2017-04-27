dabbb2bf7e887626a3b33fa43e6608d3380fcdb6
rixi- <rixi-@DESKTOP-TP4C8BO>
2017-04-27 12:16:30 +0200

    commit por Victor

---
+++ src/ed301proyectogibhub/programadorD.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed301proyectogibhub;
+
+/**
+ *
+ * @author rixi-
+ */
+public class programadorD {
+
+    public programadorD() {
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    // lokillos
+}
