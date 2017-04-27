488eefe27d3dadceac15b40c3e1a7524c82ff03c
Abenit12 <Abenit12@192.168.1.10>
2017-04-27 12:16:10 +0200

    Tomen mi trabajo, amigos :)

---
+++ src/ed301proyectogibhub/Clase_B.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed301proyectogibhub;
+
+/**
+ *
+ * @author Abenit12
+ */
+public class Clase_B {
+    /*
+    soy guay jajaja xd
+    */
+}
