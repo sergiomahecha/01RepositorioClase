537d968e86867488b03eee63df775322bb26316d
sergio <sergiohurtadomahecha@gmail.com>
2017-04-27 12:16:08 +0200

    Commit sergio

---
+++ src/ed301proyectogibhub/ClaseA.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed301proyectogibhub;
+
+/**
+ *
+ * @author sergio
+ */
+public class ClaseA {
+    private String a;
+}
